4927c1f4343ec6e17c60b44536d3a9c86a661070
Khyati Agarwal <126346169+KhyatiAgarwal-design@users.noreply.github.com>
2025-06-30 22:12:58 +0530

    Adding week2 hands on
    Nunit and Moq

---
+++ Week2/Week2_NUnit and Moq/2_Write Testable Code with Moq/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            // Example email and message
+            return _mailSender.SendMail("cust123@abc.com", "Some Message");
+        }
+    }
+}
