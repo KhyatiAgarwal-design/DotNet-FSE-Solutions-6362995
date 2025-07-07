dfa3a2ff5ddb00dab20865e68d121bc343106261
Khyati Agarwal <126346169+KhyatiAgarwal-design@users.noreply.github.com>
2025-07-07 14:46:42 +0530

    Adding week3 hands on
    Entity Framework Core 8.0: Lab1 and Lab2

---
+++ Week3/1_Lab 1 Understanding ORM with a Retail Inventory System/Code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retail Inventory System Starting...");
+
+            using var context = new RetailContext();
+            context.Database.EnsureCreated(); // Auto-create DB if not exists
+
+            Console.WriteLine("Database created successfully.");
+        }
+    }
+}
